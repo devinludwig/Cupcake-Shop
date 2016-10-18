18a8da2a591f91b51b3fe939d86b8bee7caa3bb2
Adam Baker <adam.baker97202@gmail.com>
2016-10-18 11:53:28 -0700

    add jquery for clickable paragraphs that highlight and add borders.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#introid").click(function() {
+    $("#introid").addClass("highlight");
+  });
+  $("#second").click(function() {
+    $("#second").addClass("highlight");
+  });
+  $("#third").click(function() {
+    $("#third").addClass("highlight");
+  });
+});
